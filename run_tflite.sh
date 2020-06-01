08dd549e10ffe16e2c3e89c65d554d885f850a91
Saideep <saitiku@gmail.com>
2020-05-31 18:16:39 -0700

    updated LD_PRELOAD

--- run_tflite.sh
+++ run_tflite.sh
@@ -6,26 +6,26 @@ unset LD_PRELOAD
 
 echo "Running benchmarks"
 
-./benchmark_model --num_threads=1 --warmup_runs=1 --num_runs=300 --run_delay=0.5 --graph=models/tflite/full_model.tflite 
+./benchmark_model --num_threads=1 --warmup_runs=1 --num_runs=100 --graph=models/tflite/full_model.tflite 
 
 echo "finished running full model; press any key to continue"
-read ""
+read
 
-./benchmark_model --num_threads=1 --warmup_runs=1 --num_runs=300 --run_delay=0.5 --graph=models/tflite/EEA1.tflite
+./benchmark_model --num_threads=1 --warmup_runs=1 --num_runs=100 --graph=models/tflite/EEA1.tflite
 
 echo "finished running EEA1 model; press any key to continue"
-read ""
+read
 
 
-./benchmark_model --num_threads=1 --warmup_runs=1 --num_runs=300 --run_delay=0.5 --graph=models/tflite/EEA2.tflite 
+./benchmark_model --num_threads=1 --warmup_runs=1 --num_runs=100 --graph=models/tflite/EEA2.tflite 
 
 echo "finished running EEA2 model; press any key to continue"
-read ""
+read
 
-./benchmark_model --num_threads=1 --warmup_runs=1 --num_runs=300 --run_delay=0.5 --graph=models/tflite/EEA2_EEL.tflite
+./benchmark_model --num_threads=1 --warmup_runs=1 --num_runs=100 --graph=models/tflite/EEA2_EEL.tflite
 
 echo "finished running  EEA2 Exit Branch Conv layer; press any key to EXIT"
-read ""
+read
 
 
 # reset LD_PRELOAD
